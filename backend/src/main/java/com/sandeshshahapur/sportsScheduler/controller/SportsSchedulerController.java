a6480d1fd3ef57c4ea395f539c58056e43ee0cb1
Sandesh Shahapur <sandeshshahapur@gmail.com>
2024-05-02 21:01:56 +0530

    un-gitignored target - for deployement through dockerfile

--- backend/src/main/java/com/sandeshshahapur/sportsScheduler/controller/SportsSchedulerController.java
+++ backend/src/main/java/com/sandeshshahapur/sportsScheduler/controller/SportsSchedulerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins="https://sandeshshahapur.github.io/")
+@CrossOrigin(origins="https://sandeshshahapur.github.io")
 @RestController
 public class SportsSchedulerController {
 
