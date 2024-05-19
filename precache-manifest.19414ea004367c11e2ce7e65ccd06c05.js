93f8cb7d5a456d66d37c5b261b647c0f0e3adda5
Sandesh Shahapur <sandeshshahapur@gmail.com>
2024-05-19 23:41:11 +0530

    Updates

---
+++ precache-manifest.19414ea004367c11e2ce7e65ccd06c05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8a7081e103c634d96467d280d39095",
+    "url": "/sportsScheduler/index.html"
+  },
+  {
+    "revision": "499e38ca6b10f7daef5b",
+    "url": "/sportsScheduler/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "2df5d1e6e013b43bd553",
+    "url": "/sportsScheduler/static/css/main.1eb4a2a9.chunk.css"
+  },
+  {
+    "revision": "499e38ca6b10f7daef5b",
+    "url": "/sportsScheduler/static/js/2.eb4eb096.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/sportsScheduler/static/js/2.eb4eb096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f6b689b3435993093a",
+    "url": "/sportsScheduler/static/js/3.331ae4c8.chunk.js"
+  },
+  {
+    "revision": "2df5d1e6e013b43bd553",
+    "url": "/sportsScheduler/static/js/main.de24cbcd.chunk.js"
+  },
+  {
+    "revision": "9b046d56bfa90d860329",
+    "url": "/sportsScheduler/static/js/runtime-main.1f828529.js"
+  }
+]);
